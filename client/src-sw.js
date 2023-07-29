7879b2c942b66704d357c827ed3146ae555a1931
Dinh Nguyen <nguyen_dinh282@yahoo.com>
2023-07-29 10:10:06 -0400

    add image to array of callback func of src-sw.js file so that images will be cached so icon will load when app goes offline

--- client/src-sw.js
+++ client/src-sw.js
@@ -30,8 +30,8 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 // TODO: Implement asset caching
 registerRoute(
 
-// Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
-({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+// Here we define the callback function that will filter the requests we want to cache (in this case, CSS, JS, worker, and image files)
+({ request }) => ['style', 'script', 'worker', 'image'].includes(request.destination),
 new StaleWhileRevalidate({
   // Name of the cache storage.
   cacheName: 'asset-cache',
